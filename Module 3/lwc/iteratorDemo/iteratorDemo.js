b5b0e66a57a90cb14149d452ac3de4a76c1c1098
Harsha-Chadalavada <74479878+Harsha-Chadalavada@users.noreply.github.com>
2020-11-19 19:30:50 +0800

    Module 3

---
+++ Module 3/lwc/iteratorDemo/iteratorDemo.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorDemo extends LightningElement {
+
+    contacts = [
+        {
+            Id: "C1",
+            Name: 'Amy Taylor',
+            Email: 'Amy@sheldon.com',
+        },
+        {
+            Id: "C2",
+            Name: 'Michael Jones',
+            Email: 'Micheal@jakcson.com',
+        },
+        {
+            Id: "C3",
+            Name: 'Jennifer Wu',
+            Email: 'Jennifer@jack.com',
+        },
+        {
+            Id: "C4",
+            Name: 'Peter Parker',
+            Email: 'peet@spidy.com',
+        },
+    ];
+
+}
